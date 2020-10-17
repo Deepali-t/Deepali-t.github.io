d31a3928f9a64f9cac35836042c4f91916010186
Deepali-t <deepalitripathi555@gmail.com>
2020-10-17 19:25:09 +0530

    Updates

---
+++ precache-manifest.1a67c54ff200837ae5d8f65e62ae3f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e419dd710b77682555f37186ce427467",
+    "url": "/Deepali-t/Deepali-t.github.io/index.html"
+  },
+  {
+    "revision": "8617c8a74122d88dd12b",
+    "url": "/Deepali-t/Deepali-t.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ccce22119433b0e2fdb",
+    "url": "/Deepali-t/Deepali-t.github.io/static/js/2.6e838de3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Deepali-t/Deepali-t.github.io/static/js/2.6e838de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8617c8a74122d88dd12b",
+    "url": "/Deepali-t/Deepali-t.github.io/static/js/main.508e5099.chunk.js"
+  },
+  {
+    "revision": "f765c98a79c46020159d",
+    "url": "/Deepali-t/Deepali-t.github.io/static/js/runtime-main.9e879241.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Deepali-t/Deepali-t.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
